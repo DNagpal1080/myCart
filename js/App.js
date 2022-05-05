346939c77c377eb60dc0578e1e6b513c48d0acc0
DNagpal1080 <47717468+DNagpal1080@users.noreply.github.com>
2022-05-05 12:42:30 +0530

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,3 +1,15 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
+function getData() {
+    let dataContainer = document.getElementById("ApiData");
+    let parsedData = ""
+    fetch('https://reqres.in/api/users?page=2')
+        .then(response => response.json())
+        .then(json => {
+            parsedData = json.data;
+
+            for (key of parsedData) {
+                dataContainer.innerHTML += key.email +"<br>";
+            }
+        })
+}
+
+getData();
